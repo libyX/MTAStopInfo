f3281711419784de5bbc606806749c5daa2f4cd5
Rony Xavier <rx294@nyu.edu>
2017-11-18 19:25:50 -0500

    Add a simple Sinatra application

--- app copy.rb
+++ app copy.rb
@@ -2,12 +2,10 @@
 require 'sinatra'
 require 'bcrypt'
 require 'securerandom'
-require_relative 'local_mongo_helper.rb'
-require_relative 'saas_mongo_helper.rb'
+require_relative 'mongo_helper.rb'
 
 configure do
-  @@local_mongo_handle = Mongo_DB_LOCAL.new
-  @@saas_mongo_handle = Mongo_DB_SAAS.new
+  @@mongo_handle = Mongo_DB.new
 end
 before do
   begin
@@ -22,9 +20,7 @@ before do
 end
 
 post '/signup' do
-  token = @@local_mongo_handle.signup(@request_payload[:username],
-                              @request_payload[:password])
-  		  @@saas_mongo_handle.signup(@request_payload[:username],
+  token = @@mongo_handle.signup(@request_payload[:username],
                               @request_payload[:password])
 
   return {:status => "pass", :token => token}.to_json
@@ -32,10 +28,8 @@ end
 
 
 post '/login' do
-  token = @@local_mongo_handle.login(@request_payload[:username],
-                               		 @request_payload[:password])
-  		  @@saas_mongo_handle.login(@request_payload[:username],
-                               		 @request_payload[:password])
+  token = @@mongo_handle.login(@request_payload[:username],
+                               @request_payload[:password])
   if token
     {:status => "pass", :token => token}.to_json
   else
@@ -44,8 +38,7 @@ post '/login' do
 end
 
 post '/logout' do
-  status = @@local_mongo_handle.logout(@request_payload[:username])
-  		   @@saas_mongo_handle.logout(@request_payload[:username])
+  status = @@mongo_handle.logout(@request_payload[:username])
 
   if status
     return {:status => "pass"}.to_json
@@ -55,19 +48,16 @@ post '/logout' do
 end
 
 post '/addstop' do
-  auth_status = @@local_mongo_handle.authenticate(@request_payload[:username],
-                                            	  @request_payload[:token])
+  auth_status = @@mongo_handle.authenticate(@request_payload[:username],
+                                            @request_payload[:token])
   
   if !auth_status 
     return {:status => "fail",:message => "Invalid Username or Session Key"}.to_json
   end
 
-  status = @@local_mongo_handle.add_stop(@request_payload[:username],
-                                   		 @request_payload[:stop][:name],
-                                   		 @request_payload[:stop][:code])
-  		   @@saas_mongo_handle.add_stop(@request_payload[:username],
-                                   		@request_payload[:stop][:name],
-                                   		@request_payload[:stop][:code])
+  status = @@mongo_handle.add_stop(@request_payload[:username],
+                                   @request_payload[:stop][:name],
+                                   @request_payload[:stop][:code])
   if status
     return {:status => "pass" }.to_json
   else
@@ -76,14 +66,14 @@ post '/addstop' do
 end
 
 post '/getstops' do
-  auth_status = @@local_mongo_handle.authenticate(@request_payload[:username],
+  auth_status = @@mongo_handle.authenticate(@request_payload[:username],
                                           @request_payload[:token])
   
   if !auth_status 
     return {:status => "fail",:message => "Invalid Username or Session Key"}.to_json
   end
 
-  stops = @@local_mongo_handle.get_stops(@request_payload[:username])
+  stops = @@mongo_handle.get_stops(@request_payload[:username])
 
   if stops
     return {:status => "pass", :data => stops}.to_json
@@ -93,16 +83,14 @@ post '/getstops' do
 end
 
 post '/removestop' do
-  auth_status = @@local_mongo_handle.authenticate(@request_payload[:username],
+  auth_status = @@mongo_handle.authenticate(@request_payload[:username],
                                           @request_payload[:token])
   
   if !auth_status 
     return {:status => "fail",:message => "Invalid Username or Session Key"}.to_json
   end
 
-  status = @@local_mongo_handle.delete_stop(@request_payload[:username],
-                                   @request_payload[:key])
-  		   @@saas_mongo_handle.delete_stop(@request_payload[:username],
+  status = @@mongo_handle.delete_stop(@request_payload[:username],
                                    @request_payload[:key])
   if status
     return {:status => "pass" }.to_json
@@ -112,18 +100,14 @@ post '/removestop' do
 end
 
 post '/updatestop' do
-  auth_status = @@local_mongo_handle.authenticate(@request_payload[:username],
+  auth_status = @@mongo_handle.authenticate(@request_payload[:username],
                                           @request_payload[:token])
   
   if !auth_status 
     return {:status => "fail",:message => "Invalid Username or Session Key"}.to_json
   end
 
-  status = @@local_mongo_handle.update_stop(@request_payload[:username],
-                                    @request_payload[:key],
-                                    @request_payload[:stop][:name],
-                                    @request_payload[:stop][:code])
-  status = @@saas_mongo_handle.update_stop(@request_payload[:username],
+  status = @@mongo_handle.update_stop(@request_payload[:username],
                                     @request_payload[:key],
                                     @request_payload[:stop][:name],
                                     @request_payload[:stop][:code])
---
+++ mongo_helper copy.rb
@@ -0,0 +1,124 @@
+require 'mongo'
+require 'json'
+require "base64"
+require 'securerandom'
+require 'bcrypt'
+
+class Mongo_DB_LOCAL
+  def initialize
+    @client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'mtaprofiles')
+  end
+
+  def test
+    return 'asd'
+  end
+
+  def signup(username,password)
+    coll = @client[username]
+    password_salt = BCrypt::Engine.generate_salt
+    password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    doc = {"passwordhash" => password_hash,"salt" => password_salt, "token"=> generate_token ,"data":{}}
+    coll.insert_one(doc)
+    doc["token"]
+  end
+
+  def logout(username)
+    return false unless @client.database.collection_names.include?(username)
+    coll = @client[username]
+    doc = coll.find.first
+    doc['token'] = nil
+    coll.update_one({"_id" => doc["_id"]}, doc)
+    return true
+  end
+
+  def get_user(username)
+    return false unless @client.database.collection_names.include?(username)
+    coll = @client[username]
+    coll.find.first
+  end
+
+  def login(username,password)
+    return false unless @client.database.collection_names.include?(username)
+    coll = @client[username]
+    doc = coll.find.first
+
+    if doc['passwordhash'] == BCrypt::Engine.hash_secret(password, doc['salt'])
+      doc['token'] = generate_token
+      coll.update_one({"_id" => doc["_id"]}, doc)
+      doc["token"]
+    else
+      return false
+    end
+  end
+
+  def authenticate(username,token)
+    return false unless @client.database.collection_names.include?(username)
+    coll = @client[username]
+    doc = coll.find.first
+
+    if doc['token'] == token
+      return true
+    else
+      return false
+    end
+  end
+
+  def get_stops(username)
+    return false unless @client.database.collection_names.include?(username)
+    coll = @client[username]
+    coll.find.first['data']
+  end
+
+  def add_stop(username,stop_name,stop_code)
+    return false unless @client.database.collection_names.include?(username)
+    coll = @client[username]
+    doc = coll.find.first
+    # stop.JSON.parse!
+    # puts stop
+    doc['data'][SecureRandom.urlsafe_base64(8)] = {'name'=>stop_name,'code'=>stop_code}
+    coll.update_one({"_id" => doc["_id"]}, doc)
+    return true
+  end
+
+  def update_stop(username,key,stop_name,stop_code)
+    return false unless @client.database.collection_names.include?(username)
+    coll = @client[username]
+    doc = coll.find.first
+    doc['data'][key] = {'name'=>stop_name,'code'=>stop_code}
+    coll.update_one({"_id" => doc["_id"]}, doc)
+    return true
+  end
+
+  def delete_stop(username,key)
+    return false unless @client.database.collection_names.include?(username)
+    coll = @client[username]
+    doc = coll.find.first
+    doc['data'].delete(key)
+    coll.update_one({"_id" => doc["_id"]}, doc)
+    return true
+  end
+
+  def drop_all_collections
+    json = {'collections' => []}
+    db = @client.database
+    db.collection_names.each do |collection|
+      db[collection].drop()
+    end
+  end
+
+  def generate_token
+    SecureRandom.urlsafe_base64(64)
+  end
+end
+
+# db = Mongo_DB.new
+# # db.test
+# # db.drop_all_collections
+# # db.signup('test3','Test3')
+# # token = db.login('test3','Test3')
+# # puts db.authenticate('test3',token)
+# # doc = db.get_user('test')
+# # stop = {"name"=>"name2","code"=>"10002"}
+# # db.add_stop('test3',stop)
+# # doc = db.update_stop('test','s7i6BS5clCM',stop)
+# puts db.get_stops('test3')
